1c4ac2ba94ddae9c7a104f4316798d1ef027e55d
awesomejiawei <786788745@qq.com>
2019-06-19 16:54:51 +0800

    add math

--- CodeWar.py
+++ CodeWar.py
@@ -13,4 +13,20 @@ def make_readable(seconds):
     return '{:02}:{:02}:{:02}'.format(hours, minutes, seconds)
 
 
-print(make_readable(3412))
+# Your task is to check wheter a given integer is a perfect power. If it is a perfect power,
+# return a pair m and k with mk = n as a proof. Otherwise return Nothing, Nil, null, NULL, None or your language's equivalent.
+#
+# Note: For a perfect power, there might be several pairs. For example 81 = 3^4 = 9^2, so (3,4) and (9,2) ar
+# e valid solutions. However, the tests take care of this, so if a number is a perfect power, return any pair that proves it.
+# isPP(4) => [2,2]
+# isPP(9) => [3,2]
+# isPP(5) => None
+
+from math import log, sqrt
+
+def isPP(n):
+    for b in range(2, int(sqrt(n)) + 1):
+        e = int(round(log(n, b)))
+        if b ** e == n:
+            return [b, e]
+    return None
