d1333d8fde950594a610a455c160fe3a518b2a7f
awesomejiawei <786788745@qq.com>
2019-06-22 10:40:22 +0800

    add one

--- CodeWar.py
+++ CodeWar.py
@@ -280,4 +280,24 @@ def get_pins(observed):
     result = [r_num[i:i+len(observed)] for i in range(0, len(r_num), len(observed))]  # 按固定长度输出
     return result
 
-print(get_pins('11'))
+#print(get_pins('11'))
+
+
+#9
+# Description:
+# Create a function named divisors/Divisors that takes an integer
+# n > 1 and returns an array with all of the integer's divisors(except for 1 and ' \
+#         'the number itself), from smallest to largest. If the number is prime return ' \
+#         'the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+#
+# Example:
+# divisors(12); #should return [2,3,4,6]
+# divisors(25); #should return [5]
+# divisors(13); #should return "13 is prime"
+
+def divisors(num):
+    l = [a for a in range(2,num) if num%a == 0]
+    if len(l) == 0:
+        return str(num) + " is prime"
+    return l
+
